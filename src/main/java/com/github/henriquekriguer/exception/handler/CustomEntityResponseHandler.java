561a68a3b0d317ef0ed18484cf3f25d94d17e884
Henrique-Kriguer <henriquekriguer@gmail.com>
2026-04-14 22:20:43 -0300

    feath: defined PersonServive logical to comunicate to BD

--- src/main/java/com/github/henriquekriguer/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/com/github/henriquekriguer/exception/handler/CustomEntityResponseHandler.java
@@ -1,7 +1,7 @@
 package com.github.henriquekriguer.exception.handler;
 
 import com.github.henriquekriguer.exception.ExceptionResponse;
-import com.github.henriquekriguer.exception.UnsupportedMathOperationException;
+import com.github.henriquekriguer.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -24,13 +24,13 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(response, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(
                 new java.util.Date(),
                 ex.getMessage(),
                 request.getDescription(false)
         );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/github/henriquekriguer/services/PersonServices.java
+++ src/main/java/com/github/henriquekriguer/services/PersonServices.java
@@ -1,6 +1,9 @@
 package com.github.henriquekriguer.services;
 
+import com.github.henriquekriguer.exception.ResourceNotFoundException;
 import com.github.henriquekriguer.model.Person;
+import com.github.henriquekriguer.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,49 +17,44 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    @Autowired
+    PersonRepository repository;
+
+
     public List<Person> findAll(){
         logger.info("Finding all people!");
-        List<Person> persons = new ArrayList<Person>();
-            for (int i = 0; i < 8; i++) {
-                Person person = mockPerson(i);
-                persons.add(person);
-            }
-        return persons;
+
+        return repository.findAll();
+
     }
 
-    private Person mockPerson(int i) {
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("First name " + i);
-        person.setLastName("Last Name " + i);
-        person.setAddress("Some Address in Brazil " + i);
-        person.setGender("Male");
-        return person;
-    }
-
-    public Person findById(String id){
+    public Person findById(Long id){
         logger.info("Finding one Person!");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Henrique");
-        person.setLastName("Kriguer");
-        person.setAddress("Guaratinguetá - SP - Brasil");
-        person.setGender("Male");
-        return person;
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this Id!"));
     }
 
     public Person create(Person person){
         logger.info("Creating a new Person!");
-        return person;
+        return repository.save(person);
 
     }public Person update(Person person){
         logger.info("Updating a Person!");
-        return person;
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this Id!"));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        return repository.save(entity);
     }
 
-    public void delete(String id){
+    public void delete(Long id){
         logger.info("Deleting a Person!");
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this Id!"));
+        repository.delete(entity);
     }
 }
