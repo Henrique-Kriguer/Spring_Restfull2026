4b7de4cf9117e96c23f7f34b5d296b98d4e14e1a
Henrique-Kriguer <henriquekriguer@gmail.com>
2026-04-16 22:39:05 -0300

    refactoring applicationProperties removing mySQL8 and in PersonController change all annotations

--- src/main/java/com/github/henriquekriguer/controllers/PersonController.java
+++ src/main/java/com/github/henriquekriguer/controllers/PersonController.java
@@ -4,6 +4,7 @@ import com.github.henriquekriguer.services.PersonServices;
 import com.github.henriquekriguer.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +17,29 @@ public class PersonController {
     private PersonServices service;
     // private PersonServices service = new PersonServices();
 
-    @RequestMapping(
-            method = RequestMethod.GET,
+    @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
     }
 
     //http://localhost:9090/person/1
-    @RequestMapping(value = "/{id}",
-        method = RequestMethod.GET,
+    @GetMapping(value = "/{id}",
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public Person findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @RequestMapping(
-        method = RequestMethod.POST,
+    @PostMapping(
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public Person create( @RequestBody Person person) {
         return service.create(person);
     }
-@RequestMapping(
-        method = RequestMethod.PUT,
+
+    @PutMapping(
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -49,13 +47,9 @@ public class PersonController {
         return service.update(person);
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public void delete(@PathVariable("id") Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
-
-
 }
