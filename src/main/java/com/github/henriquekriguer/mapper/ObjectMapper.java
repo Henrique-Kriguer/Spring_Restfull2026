cb4d01a2dc1a6e6bc667d3f78ea6353c1134bd72
Henrique-Kriguer <henriquekriguer@gmail.com>
2026-05-01 15:54:40 -0300

    feath: Implemens MapperTest

--- src/main/java/com/github/henriquekriguer/mapper/ObjectMapper.java
+++ src/main/java/com/github/henriquekriguer/mapper/ObjectMapper.java
@@ -12,7 +12,8 @@ public class ObjectMapper {
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
     public static <O, D> D parseObject(O origin, Class<D> destination){
-       return mapper.map(origin, destination);
+
+        return mapper.map(origin, destination);
     }
     public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination){
 
