2ea565b85e61d4dd85ce429866b5bc8415d4dc89
Henrique-Kriguer <henriquekriguer@gmail.com>
2026-05-19 20:22:30 -0300

    fix: separated migrations with flyway in two files

---
+++ src/main/resources/db/migration/V2__Populate_table_person.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'Guaratinguetá , 232 - SP - Brasil', 'Henrique', 'Male', 'The Boss - Kriguer '),
+    (3, 'Guaratinguetá - SP - Brasil', 'Francisco', 'Male', 'Kriguer'),
+    (4, 'Guaratinguetá - SP - Brasil', 'João', 'Male', 'Kriguer'),
+    (5, 'Guaratinguetá - SP - Brasil', 'Simone', 'Male', 'Kriguer'),
+    (9, 'Londres , Great Britain', 'Billy', 'Male', 'Idol - Pop rock music'),
+    (11, 'New York , USA', 'Elvis', 'Male', 'Presley'),
+    (14, 'Chicago, USA', 'Milles', 'Male', 'Davis'),
+    (20, 'Paris, France', 'Jean Luc', 'Male', 'Ponty');
+
+
