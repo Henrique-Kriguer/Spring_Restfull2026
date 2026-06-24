76fda0c65ada128621563ede1d3897051cf769e8
Henrique-Kriguer <henriquekriguer@gmail.com>
2026-06-23 23:21:43 -0300

    refactor:repository - exclude create() method

---
+++ src/main/java/com/github/henriquekriguer/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.github.henriquekriguer.config;
+/*
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+*/
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+
+public class OpenApiConfig {
+/*
+
+    @Bean
+    OpenAPI customAPI(){
+        OpenAPI v1 = new OpenAPI()
+                .info(new Info()
+                        .title("REST API´s RESTful")
+                        .version("V1")
+                        .description("Person Service REST API: A lightweight JSON API for managing Person resources. It offers CRUD endpoints (GET /persons, GET /persons/{id}, POST /persons, PUT/PATCH /persons/{id}, DELETE /persons/{id}) with pagination, filtering, input validation, and standard HTTP status codes. Optional authentication (e.g., JWT) and clear error responses make it suitable for user profiles and contact management.")
+                        .termsOfService("https://github.com/Henrique-Kriguer")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("git@github.com:Henrique-Kriguer/Spring_Restfull2026.git")
+                        )
+                );
+        return v1;
+
+    }*/
+}
--- src/main/java/com/github/henriquekriguer/repository/PersonRepository.java
+++ src/main/java/com/github/henriquekriguer/repository/PersonRepository.java
@@ -4,5 +4,5 @@ import com.github.henriquekriguer.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    Object create();
+   // Object create();
 }
